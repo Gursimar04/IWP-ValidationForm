8cfffe5f8b872aea5b478a37cc5b3e76160921c0
gursimar04 <gursimar04@gmail.com>
2020-09-12 17:51:37 +0530

    Updates

---
+++ precache-manifest.df314e7c785bdf1413e3d59db28064a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a30e6d4361229774381e07d15836c2",
+    "url": "/IWP-ValidationForm/index.html"
+  },
+  {
+    "revision": "e7273be64d59458eb65e",
+    "url": "/IWP-ValidationForm/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "585a57bf1747d7033b96",
+    "url": "/IWP-ValidationForm/static/js/2.42d44309.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/IWP-ValidationForm/static/js/2.42d44309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7273be64d59458eb65e",
+    "url": "/IWP-ValidationForm/static/js/main.2dfc42f6.chunk.js"
+  },
+  {
+    "revision": "ba659e7032a706d9241e",
+    "url": "/IWP-ValidationForm/static/js/runtime-main.3930c837.js"
+  }
+]);
